07ff65e25115908ab70ab43f580f0d2dd8fa16e5
Christian Baroni <7061887+christianbaroni@users.noreply.github.com>
2024-11-14 01:07:19 -0500

    Add all haptic types to example apps

--- package/example-paper/App.tsx
+++ package/example-paper/App.tsx
@@ -9,8 +9,38 @@ function App(): React.JSX.Element {
       <StatusBar barStyle="light-content" />
       <GestureHandlerRootView>
         <View style={styles.root}>
+          <GestureHandlerButton hapticType="impactLight" style={styles.button}>
+            <Text style={styles.text}>impactLight</Text>
+          </GestureHandlerButton>
+          <GestureHandlerButton hapticType="impactMedium" style={styles.button}>
+            <Text style={styles.text}>impactMedium</Text>
+          </GestureHandlerButton>
+          <GestureHandlerButton hapticType="impactHeavy" style={styles.button}>
+            <Text style={styles.text}>impactHeavy</Text>
+          </GestureHandlerButton>
+          <GestureHandlerButton
+            hapticType="notificationError"
+            style={styles.button}>
+            <Text style={styles.text}>notificationError</Text>
+          </GestureHandlerButton>
+          <GestureHandlerButton
+            hapticType="notificationWarning"
+            style={styles.button}>
+            <Text style={styles.text}>notificationWarning</Text>
+          </GestureHandlerButton>
+          <GestureHandlerButton
+            hapticType="notificationSuccess"
+            style={styles.button}>
+            <Text style={styles.text}>notificationSuccess</Text>
+          </GestureHandlerButton>
+          <GestureHandlerButton hapticType="rigid" style={styles.button}>
+            <Text style={styles.text}>rigid</Text>
+          </GestureHandlerButton>
+          <GestureHandlerButton hapticType="selection" style={styles.button}>
+            <Text style={styles.text}>selection</Text>
+          </GestureHandlerButton>
           <GestureHandlerButton hapticType="soft" style={styles.button}>
-            <Text style={styles.text}>Tap Me</Text>
+            <Text style={styles.text}>soft</Text>
           </GestureHandlerButton>
         </View>
       </GestureHandlerRootView>
@@ -26,12 +56,14 @@ const styles = StyleSheet.create({
     borderRadius: 24,
     height: 48,
     justifyContent: 'center',
-    paddingHorizontal: 24,
+    paddingHorizontal: 20,
   },
   root: {
     alignItems: 'center',
     backgroundColor: '#000000',
     flex: 1,
+    flexDirection: 'column',
+    gap: 28,
     justifyContent: 'center',
   },
   text: {
